930cc52a46661f2b73550ab1a73ae005f8d315fd
unknown <lehongquan15a@gmail.com>
2020-12-02 22:05:13 +0700

    Create some folder to start

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const port = process.env.PORT||3000;
 app.get('/', function (req, res) {
   res.send('Hello World');
 })
@@ -8,3 +13,4 @@ app.get('/', function (req, res) {
 app.listen(port,()=>{
     console.log('Server start at '+port);
 });
+
